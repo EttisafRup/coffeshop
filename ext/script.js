e643329b1df6d45e7c58ad8e453ea299d3fb42e2
Ettisaf Rup <107711625+EttisafRup@users.noreply.github.com>
2022-07-30 17:04:17 +0600

    Add files via upload

---
+++ ext/script.js
@@ -0,0 +1,10 @@
+// TypeScript Declearation 
+const options = {
+    strings: ["your family!", "your friends!","someone special..!"],
+    typeSpeed: 40,
+    backDelay: 3000,
+    backSpeed: 40,
+    loop: true,
+  };  
+
+  const typed = new Typed('.typed', options);
